1f0687e7da8bbbacc26c2a84d4e905acc83ed37e
Lewko0 <anna.luginets@gmail.com>
2024-02-22 18:28:14 +0300

    sess

--- flask/main.py
+++ flask/main.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from data import db_session
+from data.users import User
+from data.jobs import Jobs
 
 
 app = Flask(__name__)
@@ -8,7 +10,19 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 def main():
     db_session.global_init("db/mars_explorer.db")
-    app.run()
+    # app.run()
+    user = User()
+    user.surname = "Scott"
+    user.name = "Ridley"
+    user.age = 21
+    user.position = "captain"
+    user.speciality = "research engineer"
+    user.address = "module_1"
+    user.email = "scott_chief@mars.org"
+    user.hashed_password = "cap"
+    db_sess = db_session.create_session()
+    db_sess.add(user)
+    db_sess.commit()
 
 
 if __name__ == '__main__':
